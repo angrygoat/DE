4647f970435f632aeccf76df8b3b4922fd8b7d5f
michael-conway <michael_conway@unc.edu>
2016-02-29 14:42:40 -0500

    #1 add build of docker config images

--- ansible/build_config_images.sh
+++ ansible/build_config_images.sh
@@ -1,29 +1,27 @@
 #!/usr/bin/env bash
 
-docker build -t diceunc/config_anon_files .
-docker build -t diceunc/config_apps .
-docker build -t diceunc/config_iplant_data_apps .
-docker build -t diceunc/config_clockwork .
-docker build -t diceunc/config_clm .
-docker build -t diceunc/config_data_info .
-docker build -t diceunc/config_dewey .
-docker build -t diceunc/config_de_ui .
-docker build -t diceunc/config_iplant_data_de_ui .
-docker build -t diceunc/config_de_ui_nginx .
-docker build -t diceunc/config_iplant_data_de_ui_nginx .
-docker build -t diceunc/config_exim_sender .
-docker build -t diceunc/config_info_typer .
-docker build -t diceunc/config_infosquito .
-docker build -t diceunc/config_iplant_email .
-docker build -t diceunc/config_iplant_groups .
-docker build -t diceunc/config_jex_events .
-docker build -t diceunc/config_kifshare .
-docker build -t diceunc/config_metadata .
-docker build -t diceunc/config_monkey .
-docker build -t diceunc/config_notification_agent .
-docker build -t diceunc/config_saved_searches .
-docker build -t diceunc/config_terrain .
-docker build -t diceunc/config_iplant_data_terrain .
-docker build -t diceunc/config_tree_urls .
-docker build -t diceunc/config_user_preferences .
-docker build -t diceunc/config_user_sessions .
+docker build -t diceunc/config_anon_files -f DockerfileConfigs .
+docker build -t diceunc/config_apps -f DockerfileConfigs  .
+docker build -t diceunc/config_iplant_data_apps -f DockerfileConfigs .
+docker build -t diceunc/config_clockwork -f DockerfileConfigs .
+docker build -t diceunc/config_clm -f DockerfileConfigs .
+docker build -t diceunc/config_data_info -f DockerfileConfigs .
+docker build -t diceunc/config_dewey -f DockerfileConfigs .
+docker build -t diceunc/config_de_ui -f DockerfileConfigs .
+docker build -t diceunc/config_iplant_data_de_ui -f DockerfileConfigs .
+docker build -t diceunc/config_de_ui_nginx -f DockerfileConfigs .
+docker build -t diceunc/config_iplant_data_de_ui_nginx -f DockerfileConfigs .
+docker build -t diceunc/config_exim_sender -f DockerfileConfigs .
+docker build -t diceunc/config_info_typer -f DockerfileConfigs .
+docker build -t diceunc/config_iplant_email -f DockerfileConfigs .
+docker build -t diceunc/config_jex_events -f DockerfileConfigs .
+docker build -t diceunc/config_kifshare -f DockerfileConfigs .
+docker build -t diceunc/config_metadata -f DockerfileConfigs .
+docker build -t diceunc/config_monkey -f DockerfileConfigs .
+docker build -t diceunc/config_notification_agent -f DockerfileConfigs .
+docker build -t diceunc/config_saved_searches -f DockerfileConfigs .
+docker build -t diceunc/config_terrain -f DockerfileConfigs .
+docker build -t diceunc/config_iplant_data_terrain -f DockerfileConfigs .
+docker build -t diceunc/config_tree_urls -f DockerfileConfigs .
+docker build -t diceunc/config_user_preferences -f DockerfileConfigs .
+docker build -t diceunc/config_user_sessions -f DockerfileConfigs .
