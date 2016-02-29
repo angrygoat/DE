312dd1395435473f9e8dc2c8c28c549dca93aa4c
michael-conway <michael_conway@unc.edu>
2016-02-29 16:44:21 -0500

    #1 add build of docker config images

--- ansible/build_config_images.sh
+++ ansible/build_config_images.sh
@@ -25,3 +25,28 @@ docker build -t diceunc/config_iplant_data_terrain -f DockerfileConfigs .
 docker build -t diceunc/config_tree_urls -f DockerfileConfigs .
 docker build -t diceunc/config_user_preferences -f DockerfileConfigs .
 docker build -t diceunc/config_user_sessions -f DockerfileConfigs .
+
+
+docker push diceunc/config_anon_files
+docker push diceunc/config_apps
+docker push  diceiunc/config_iplant_data_apps
+docker push  diceunc/config_data_info
+docker  push  diceunc/config_dewey
+docker  push  diceunc/config_de_ui
+docker  push  diceunc/config_iplant_data_de_ui
+docker  push  diceunc/config_de_ui_nginx
+docker  push  diceunc/config_iplant_data_de_ui_nginx
+docker  push  diceunc/config_exim_sender
+docker push  diceunc/config_info_typer
+docker push  diceunc/config_iplant_email
+docker  push  diceunc/config_jex_events
+docker  push  diceunc/config_kifshare
+docker  push  diceunc/config_metadata
+docker push  diceunc/config_monkey
+docker  push  diceunc/config_notification_agent
+docker  push  diceunc/config_saved_searches
+docker  push  diceunc/config_terrain
+docker  push  diceunc/config_iplant_data_terrain
+docker push  diceunc/config_tree_urls
+docker push diceunc/config_user_preferences
+docker  push  diceunc/config_user_sessions
