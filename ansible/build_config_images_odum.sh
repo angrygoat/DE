7230da715dbff4d3c7d3e1fe27af2fea20cc9a76
don sizemore <don.sizemore@gmail.com>
2016-03-10 14:43:34 -0500

    #14 use de not happygoat

--- ansible/build_config_images_odum.sh
+++ ansible/build_config_images_odum.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker build -t docker.irss.unc.edu/config_happygoat -f DockerfileConfigs .
-docker push docker.irss.unc.edu/config_happygoat
+docker build -t docker.irss.unc.edu/config_de -f DockerfileConfigs .
+docker push docker.irss.unc.edu/config_de
