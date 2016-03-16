2693b15e298820e8c0f1a3ce68d6939fd9b9350a
Lisa Stillwell <lisa@renci.org>
2016-03-16 13:53:46 -0400

    updates for gitignore and build renci config images

--- ansible/build_config_images_renci.sh
+++ ansible/build_config_images_renci.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker build -t angrygoat.irss.unc.edu/config_happygoat -f DockerfileConfigs .
-docker push angrygoat.irss.unc.edu/config_happygoat
+docker build -t angrygoat.irss.unc.edu/config_de -f DockerfileConfigs .
+docker push angrygoat.irss.unc.edu/config_de
