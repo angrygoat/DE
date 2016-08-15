a8dd11189be28cbb26eef8846876201a12e11efb
michael-conway <michael_conway@unc.edu>
2016-08-15 13:55:08 -0400

    #30 add version number to config image

--- ansible/build_config_images_dfc.sh
+++ ansible/build_config_images_dfc.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker build -t dfc-de-db.edc.renci.org/config_de -f DockerfileConfigs .
-docker push dfc-de-db.edc.renci.org/config_de
+docker build -t dfc-de-db.edc.renci.org/config_de:2.5 -f DockerfileConfigs .
+docker push dfc-de-db.edc.renci.org/config_de:2.5
